8dc2879e75fe20f78e656c47232bd242e88e0b0c
Sam <sammobrowne@gmail.com>
2024-05-01 23:41:27 +0800

    postcard module

---
+++ src/app/postcard/postcard.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PostcardComponent } from './components/postcard/postcard.component';
+
+
+
+@NgModule({
+  declarations: [
+    PostcardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PostcardModule { }
