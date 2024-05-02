8295340281988e8850413ed101587d280461f9a4
Sam <sammobrowne@gmail.com>
2024-05-02 15:51:57 +0800

    postcard form lol

--- src/app/postcard/components/postcard/postcard.component.ts
+++ src/app/postcard/components/postcard/postcard.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-postcard',
   templateUrl: './postcard.component.html',
-  styleUrls: ['./postcard.component.css']
+  styleUrls: ['./postcard.component.css'],
 })
-export class PostcardComponent {
+export class PostcardComponent implements OnInit {
+  myForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    address: new FormControl('', Validators.required),
+  });
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      //send form...
+    }
+  }
 }
